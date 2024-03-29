e5a09606f861a4dcbefdb0c1b70818a92d63d26b
Hugo H. Lourenço <hugohlourenco@gmail.com>
2024-03-29 20:27:17 -0300

    Update Lutador.java

--- Guanabara/src/Relacionamento_entre_classes/Lutador.java
+++ Guanabara/src/Relacionamento_entre_classes/Lutador.java
@@ -1,4 +1,129 @@
 package Relacionamento_entre_classes;
 
 public class Lutador {
+    private String nome, nacionalidade, categoria;
+    private int idade, vitorias, derrotas, empates;
+    private float altura, peso;
+
+    public void apresentar(){
+
+    }
+
+    public void status(){
+
+    }
+
+    public void ganharLuta(){
+
+    }
+
+    public void perderLuta(){
+
+    }
+
+    public void empatarLuta(){
+
+    }
+
+    public Lutador() {
+    }
+
+    public Lutador(String nome, String nacionalidade, String categoria, int idade, int vitorias, int derrotas, int empates, float altura, float peso) {
+        this.nome = nome;
+        this.nacionalidade = nacionalidade;
+        this.categoria = categoria;
+        this.idade = idade;
+        this.vitorias = vitorias;
+        this.derrotas = derrotas;
+        this.empates = empates;
+        this.altura = altura;
+        this.peso = peso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getVitorias() {
+        return vitorias;
+    }
+
+    public void setVitorias(int vitorias) {
+        this.vitorias = vitorias;
+    }
+
+    public int getDerrotas() {
+        return derrotas;
+    }
+
+    public void setDerrotas(int derrotas) {
+        this.derrotas = derrotas;
+    }
+
+    public int getEmpates() {
+        return empates;
+    }
+
+    public void setEmpates(int empates) {
+        this.empates = empates;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    @Override
+    public String toString() {
+        return "Lutador{" +
+                "nome='" + nome + '\'' +
+                ", nacionalidade='" + nacionalidade + '\'' +
+                ", categoria='" + categoria + '\'' +
+                ", idade=" + idade +
+                ", vitorias=" + vitorias +
+                ", derrotas=" + derrotas +
+                ", empates=" + empates +
+                ", altura=" + altura +
+                ", peso=" + peso +
+                '}';
+    }
 }

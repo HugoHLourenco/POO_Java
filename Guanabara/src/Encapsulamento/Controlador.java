982792aec12670114c972808c86038213e0dc88f
Hugo H. Lourenço <hugohlourenco@gmail.com>
2024-03-29 16:19:07 -0300

    Update Controlador.java

--- Guanabara/src/Encapsulamento/Controlador.java
+++ Guanabara/src/Encapsulamento/Controlador.java
@@ -1,4 +1,12 @@
 package Encapsulamento;
 
-public class Controlador {
+public abstract class Controlador {
+    public abstract void ligar();
+    public abstract void desligar();
+    public abstract void abrirMenu();
+    public abstract void fecharMenu();
+    public abstract void mutar();
+    public abstract void desMutar();
+    public abstract void play();
+    public abstract void pause();
 }

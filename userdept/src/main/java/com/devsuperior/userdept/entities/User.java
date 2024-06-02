1abbad09cb72306c6a40f971e601b8d92fefd0a4
Hugo H. Lourenço <hugohlourenco@gmail.com>
2024-06-02 10:24:52 -0300

    importando

--- userdept/src/main/java/com/devsuperior/userdept/entities/User.java
+++ userdept/src/main/java/com/devsuperior/userdept/entities/User.java
@@ -1,10 +1,18 @@
 package com.devsuperior.userdept.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
 
+    @ManyToOne
     private Departament department;
 
     public User() {

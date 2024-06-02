c01db8e6f476beff630d241679a805e103dd0a49
Hugo H. Lourenço <hugohlourenco@gmail.com>
2024-06-02 11:15:45 -0300

    tentativa 3

--- treino_SpringBoot/src/main/resources/import.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO tb_departmento(nome) VALUES ('Gestão');
-INSERT INTO tb_departmento(nome) VALUES ('Informática');
-
-INSERT INTO tb_user(departmento_id, nome, email) VALUES (1, 'Maria', 'maria@gmail.com');
-INSERT INTO tb_user(departmento_id, nome, email) VALUES (1, 'Bob', 'bob@gmail.com');
-INSERT INTO tb_user(departmento_id, nome, email) VALUES (2, 'Alex', 'alex@gmail.com');
-INSERT INTO tb_user(departmento_id, nome, email) VALUES (2, 'Ana', 'ana@gmail.com');
